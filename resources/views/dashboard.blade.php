393cc133abe5b2f27a5ce1673b80856c8069ae84
luis esteves <luisedesteves@gmail.com>
2023-06-26 06:34:08 -0300

    segunda subida

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,9 +6,14 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-6">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <h1>Prueba de deploy al hacer un push!!!</h1>
+            </div>
+        </div>
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <h1>Esta es una prueba de programacion dia 24623</h1>
             </div>
         </div>
     </div>
